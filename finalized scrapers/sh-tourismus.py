b186d32920ae08ff0406b948ca2d4ef66202d281
Mareike Böckel <mareike.boeckel@singular-it.de>
2024-12-15 20:15:41 +0100

    Refactoring and commenting on the code for comprehensibility

--- finalized scrapers/sh-tourismus.py
+++ finalized scrapers/sh-tourismus.py
@@ -1,5 +1,5 @@
 # Script for Scraping SH-Tourismus Event Calendar
-# The functions scrape_sh_tourismus() and preprocess_sh_tourismus() should be imported to the script defining the main scraping process
+# The functions scrape_sh_tourismus() and preprocess_sh_tourismus() can be used for the main scraping process
 
 # Imports
 
@@ -17,29 +17,28 @@ import time
 
 # Scraping function
 
-def scrape_sh_tourismus(days_in_advance=10):
+def scrape_sh_tourismus(days_in_advance=10): # Optional parameter for how many days in advance to scrape events for
     
-    # Preparations
+    # Preparations for scraping (setting options, instantiating the Chrome driver, opening the website, defining waits)
     options = Options()
-    #options.add_argument("--headless")  # Run Chromium in headless mode
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(service=Service(), options=options)
-    #driver = webdriver.Firefox()
     driver.get('https://www.sh-tourismus.de/veranstaltungskalender')
+    wait = WebDriverWait(driver, 10)
 
-    driver.get('https://tashwhl.infomaxnet.de/event_sh-tourismus/?widgetToken=zOpiJSoCowQ.&amp;') #TODO hübsch machen statt iframe
-
+    # Open the embedded website from the iframe on the website that contains the calendar content
+    driver.get('https://tashwhl.infomaxnet.de/event_sh-tourismus/?widgetToken=zOpiJSoCowQ.&amp;')
     time.sleep(5)
-    #iframe = WebDriverWait(driver, 10).until(
-    #    EC.presence_of_element_located((By.CSS_SELECTOR, 'iframe[src*="tashwhl.infomaxnet.de"]'))
-    #)
+    #iframe = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'iframe[src*="tashwhl.infomaxnet.de"]'))) #not working in headless mode
     #driver.switch_to.frame(iframe)
 
-    wait = WebDriverWait(driver, 10)
-    clicks = days_in_advance*30 + 100 #puffer kann deutlich reduziert evtl sogar gelöscht werden TODO
+    # Defining how far to scroll down and load more events according to a heuristic to minimally cover the chosen number of days in advance 
+    clicks = days_in_advance*30 + 100
 
-    for i in range(clicks): #TODO evaluate sleeps
+    # Iterating over the number of clicks defined above and clicking on the "load more events" button that often (if possible)
+    for i in range(clicks):
         time.sleep(2)
         try:
             button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, '-IMXEVENT-lazyLoadButton')))
@@ -50,11 +49,15 @@ def scrape_sh_tourismus(days_in_advance=10):
             print("Button not found or not clickable within the timeout period.")
             break
 
+    # Finding all loaded events on the whole page, the specific web element representing an event is identified via CSS selector
     articles = driver.find_elements(By.CSS_SELECTOR, 'article.-IMXEVNT-listElement')
 
-
+    # Creating a list to store events
     events_data = []
 
+    # Iterating over all found event elements and extracting the required information
+    # As sometimes not all information is available per event, this part was made robust by using try-except statements
+    # This website has (almost) the same html structure as Hamburg.de event calendar, so also the information is retrieved the same way
     for article in articles:
         try:
             event_category = article.find_element(By.CSS_SELECTOR, 'p.-IMXEVNT-listElement__text__subline').text
@@ -77,7 +80,7 @@ def scrape_sh_tourismus(days_in_advance=10):
             parts = []
 
         if len(parts) >= 3:
-            date, time_x, location = parts[0], parts[1], ' / '.join(parts[2:]) #benennung als time ungünstig TODO
+            date, time_x, location = parts[0], parts[1], ' / '.join(parts[2:])
         elif len(parts) == 2:
             date, time_x, location = parts[0], parts[1], ''
         else:
@@ -93,6 +96,7 @@ def scrape_sh_tourismus(days_in_advance=10):
         except NoSuchElementException:
             event_source = ' '
 
+        # All information per event is stored into a dictionary and the dictionary is appended to the list of events
         events_data.append({
             'Category': event_category,
             'Title': event_title,
@@ -103,8 +107,8 @@ def scrape_sh_tourismus(days_in_advance=10):
             'Source': event_source
         })
 
+    # Last steps: Creating the dataframe of raw data from the event list, closing the driver and returning the dataframe
     df = pd.DataFrame(events_data)
-
     driver.close()
 
     return df
@@ -113,12 +117,12 @@ def scrape_sh_tourismus(days_in_advance=10):
 # Preprocessing function
 
 def preprocess_sh_tourismus(df_raw):
-
+    # Bringing the raw data into the agreed final data format (Changing column names into the agreed on final names, converting final date format from DD.MM.YYYY to YYYY-MM-DD, preprocessing start and end time,
+    # dropping not further needed columns, checking the category information for music relatedness and adding music label True or False (with helper function), filling empties with " ", sorting the columns)
     df_raw.rename(columns={'Title': 'Subject'}, inplace=True)
 
     df_raw.rename(columns={'Date': 'Start_date'}, inplace=True)
     df_raw["Start_date"] = df_raw["Start_date"].apply(convert_date_format)
-    #df_raw["Start_date"] = pd.to_datetime(df_raw["Start_date"], format='%d.%m.%Y').dt.strftime('%Y-%m-%d') #same as Eventim TODO
     df_raw["End_date"] = df_raw["Start_date"]
 
     df_raw[['Start_time', 'End_time']] = df_raw['Time'].apply(preprocess_time)
@@ -137,6 +141,7 @@ def preprocess_sh_tourismus(df_raw):
 # Helper functions and elements
 
 def preprocess_time(time_str):
+    # Helper function to process start time and, if stated, end time into correct format
     if '-' in time_str:
         start_time, end_time = time_str.split(' - ')
         end_time = end_time.replace(' Uhr', '')
@@ -145,16 +150,19 @@ def preprocess_time(time_str):
         end_time = ' '
     return pd.Series([start_time, end_time])
 
-
+# List of strings contained in music related category titles
 music = ['Konzert', 'konzert', 'Musik', 'musik', 'Party', 'party', 'Tanz', 'tanz', 'Festival', 'festival', 'Musical', 'musical', 'Jazz', 'jazz', 'Blues', 'blues', 'Country', 'country', 'Folk', 'folk', 'Rock', 'rock', 'Pop', 'pop', 'Klassik', 'klassik', 'Gospel', 'gospel', 'Chöre', 'chöre']
 
 def check_music(category):
+    # Function to check if an event is music related or not according to its category and provide the correct label
     for word in music:
         if word in category:
             return True
     return False
 
 def convert_date_format(date_str):
+    # Function for converting date format from DD.MM.YYYY to YYYY-MM-DD 
+    # Date format changed over the course of the project, so this function was added to older scrapers
     date_str = str(date_str)
     if "." in date_str:
         return pd.to_datetime(date_str, format='%d.%m.%Y').strftime('%Y-%m-%d')
@@ -162,7 +170,7 @@ def convert_date_format(date_str):
         return " "
 
 
-# Example usage
+# Recommended usage of the above functions
 
 df_raw = scrape_sh_tourismus(10)
 df_prep = preprocess_sh_tourismus(df_raw)
